db737d1a1fa75bca714c8d67dc909b79ee20ec4f
Víctor Iranzo <viranzo96@gmail.com>
2020-01-18 23:08:34 +0100

    Teminado ejercicio 4.

--- Exercise4/MultipleImages/MultipleImages/MainPage.xaml.cs
+++ Exercise4/MultipleImages/MultipleImages/MainPage.xaml.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace MultipleImages
+﻿namespace MultipleImages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Image_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            FrameworkElement elem = sender as FrameworkElement;
+
+            CompositeTransform transform = elem.RenderTransform as CompositeTransform;
+
+            transform.Rotation += e.Delta.Rotation;
+            transform.CenterX = elem.Width / 2;
+            transform.CenterY = elem.Height / 2;
+            transform.ScaleX *= e.Delta.Scale;
+            transform.ScaleY *= e.Delta.Scale;
+            transform.TranslateX += e.Delta.Translation.X;
+            transform.TranslateY += e.Delta.Translation.Y;
         }
     }
 }
